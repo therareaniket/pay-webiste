ffa23c5a49115c0c74924cd6a95e2a2079c1bc47
rajvi16-rarepixels <rajvi.rarepixelsdesign@gmail.com>
2025-11-28 18:02:20 +0530

    fix 🐛:  changed css for solution responsive

--- src/components/Bloglistingpage/BlogListingHero.tsx
+++ src/components/Bloglistingpage/BlogListingHero.tsx
@@ -1,9 +1,31 @@
-export default function BloglistingHero (){
+"use client"
+import Image from "next/image";
+import Slider from "react-slick";
+
+
+export default function BloglistingHero() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    };
     return (
         <>
-            <section className="section">
-                
-            </section>
+            <div className="container">
+                   
+                <div className="blog-hero-wrapper">
+                    <Image src="/images/bloglisting/bloghero-img-1.webp" alt="" width={1400} height={700} priority={false}></Image>
+                    <Image src="/images/bloglisting/bloghero-img-2.webp" alt="" width={1400} height={700} priority={false}></Image>
+                    <Image src="/images/bloglisting/bloghero-img-3.webp" alt="" width={1400} height={700} priority={false}></Image>
+                </div>
+              
+            </div>
         </>
     );
 }
+
+
