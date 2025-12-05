cb01a9afe44c8c0d5e4e95896386ffacbdcff101
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-12-05 22:32:14 +0530

    fix 🐛: Remove maxScale for Accessibility in Page Score

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Header from "@/components/Header";
 import Footer from "../components/Footer";
 
 
-export const viewport = { width: "device-width", initialScale: 1, maximumScale: 1 };
+export const viewport = { width: "device-width", initialScale: 1 };
 
 export const metadata: Metadata = {
     title: "DhatuPay | Document Driven, Smarter, Scalable Trials",
